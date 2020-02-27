41c0ab862c1b39400c3cebccc5a5090c6b0765b3
Tetsuya Kusumoto <kusumot2.ya.o@gmail.com>
2020-02-28 00:56:52 +0900

    initial commit

---
+++ js/data.js
@@ -0,0 +1,36 @@
+const Data = {
+  no_1: {
+    // "info window" に表示する各ポイント毎のデータ
+    "updateData": "2020年2月25日 00：00",
+    "name": "△△市役所",
+    "stat": "green",
+    "time": "08:30 ~ 17:15",
+    "address": "xx県△△市〇〇町1丁目〇〇" ,
+    "latlng": {lat: 35.2479304, lng: 136.971527},
+    "tel": "000-0000-0000",
+    "text": "test text test text test text test text test text",
+    "link": "https://exsample.com"
+  },
+  no_2: {
+    "updateData": "2020年2月25日 00：00",
+    "name": "××県△△保健所",
+    "stat": "yellow",
+    "time": "08:45 ~ 17:30",
+    "address": "××県△△市〇〇町２丁目〇〇" ,
+    "latlng":{lat: 35.2363814, lng: 136.9592425},
+    "tel": "000-0000-0000",
+    "text": "テスト テスト テスト テスト テスト テスト テスト テスト テスト ",
+    "link": "https://exsample.com"
+  },
+  no_3: {
+    "updateData": "2020年2月25日 00：00",
+    "stat": "red",
+    "name": "△△井〇〇郵便局",
+    "time": "09:00 ~ 17:00",
+    "address": "xx県△△市〇〇町〇〇〇−〇",
+    "latlng":{lat: 35.2307838, lng: 136.9517169},
+    "tel": "000-0000-0000",
+    "text": "test test test test test test test test ",
+    "link": "https://exsample.com"
+  },
+}
